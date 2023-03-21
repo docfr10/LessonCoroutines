b674973dfc058a8c52dc10b2b542e76a98d3d19d
docfr10 <docfr10@yandex.ru>
2023-03-21 14:15:13 +0300

    Added new coroutines

--- app/src/main/java/com/example/lesson_coroutines/view/AppScreen.kt
+++ app/src/main/java/com/example/lesson_coroutines/view/AppScreen.kt
@@ -9,9 +9,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 
 @Composable
 fun AppScreen() {
@@ -21,9 +20,12 @@ fun AppScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Fun1()
-        // Запуск корутины в основном потоке
+        //Fun1()
         //Fun2()
+        //Fun3()
+        //Fun4()
+        //Fun5()
+        Fun6()
     }
 }
 
@@ -52,4 +54,86 @@ fun Fun2() {
         text.value = result.await()
     }
     Text(text = text.value)
+}
+
+@Composable
+fun Fun3() {
+    val textList = remember { mutableStateListOf("") }
+    LaunchedEffect(Unit) {
+        val job = launch(start = CoroutineStart.LAZY) {
+            delay(200L)
+            textList.add("Start")
+            delay(5000L)
+        }
+        job.start()
+        job.join()
+        textList.add("Finish")
+    }
+    textList.add("Other text")
+    LazyRow(content = {
+        items(textList) {
+            Text(text = it)
+        }
+    })
+}
+
+@Composable
+fun Fun4() {
+    val textList = remember { mutableStateListOf("") }
+    LaunchedEffect(Unit) {
+        val numDeferred1 = async { 1 + 2 }
+        val numDeferred2 = async { 3 + 4 }
+        val numDeferred3 = async { 5 + 6 }
+        textList.add(numDeferred1.await().toString())
+        textList.add(numDeferred2.await().toString())
+        textList.add(numDeferred3.await().toString())
+    }
+    LazyRow(content = {
+        items(textList) {
+            Text(text = " $it")
+        }
+    })
+}
+
+@OptIn(DelicateCoroutinesApi::class)
+@Composable
+fun Fun5() {
+    val textList = remember { mutableStateListOf("") }
+    LaunchedEffect(Unit) {
+        launch(Dispatchers.Default) {
+            textList.add(Thread.currentThread().name)
+        }
+        launch(newSingleThreadContext("Custom Thread")) {
+            textList.add(Thread.currentThread().name)
+        }
+        textList.add(Thread.currentThread().name)
+    }
+    LazyRow(content = {
+        items(textList) {
+            Text(text = " $it")
+        }
+    })
+}
+
+@Composable
+fun Fun6() {
+    val textList = remember { mutableStateListOf("") }
+    LaunchedEffect(Unit) {
+        val channel = Channel<String>()
+        launch {
+            val users = listOf("Tom", "Bob", "Sam")
+            for (user in users) {
+                channel.send(user)  // Отправляем данные в канал
+            }
+            channel.close()  // Закрытие канала
+        }
+        for(user in channel) {  // Получаем данные из канала
+            textList.add(user)
+        }
+    }
+    LazyRow(content = {
+        items(textList) {
+            Text(text = " $it")
+        }
+    })
 }
--- app/src/main/java/com/example/lesson_coroutines/viewModel/AppScreenViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lesson_coroutines.viewModel
-
-import androidx.lifecycle.ViewModel
-
-class AppScreenViewModel : ViewModel() {
-
-}
